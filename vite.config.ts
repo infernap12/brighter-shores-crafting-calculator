7df39dee6a222dbfc6f6c8998103f4d5662fbd63
james <james1.6@hotmail.com>
2025-02-15 00:08:01 +1030

    Updated base configuration in Vite config file
    The base configuration has been updated in the Vite configuration file. The base has been set to "brighter-shores-weapon-calculator/", which provides a relative path for the application to understand the location of static resources. No other changes are made in this commit.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	base: "brighter-shores-weapon-calculator/",
 	resolve: {
 		alias: {
 			'@': '/src',
